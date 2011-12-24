14f0bf57fe5d7db197ecce76cf64b350544d0a8b
Timothy Klim <i@timothyklim.com>
2011-12-24 14:37:05 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yeah::Application.config.secret_token = '2348616f4980bc6195d185ad7fbe95d093dcb9c64ac64f1b5e865803079ceddb6a31c7a6c56890ffb3022a45a28a0642d0eb0eda6d7a1f0db6fc22b242546b92'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Yeah::Application.routes.draw do
+  resources :posts
+
+  root to: 'posts#index'
+end
